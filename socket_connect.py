00483ec87bb2acdfdc197314636883eb10feaf41
swilli21 <51794492+swilli21@users.noreply.github.com>
2020-09-27 17:52:20 -0400

    Update socket_connect.py
    How you can retrieve data from a server by creating sockets

--- socket_connect.py
+++ socket_connect.py
@@ -4,6 +4,7 @@ mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80))
 cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
+# you must send GET request before you can retrieve the data
 
 while True:
     data = mysock.recv(512)
